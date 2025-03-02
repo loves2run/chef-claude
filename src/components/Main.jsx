e56defa7c8bfaab8738b71943ce9230f8ce8df08
Brandon Johnson <johnsbr99@gmail.com>
2025-03-02 16:20:42 -0700

    Added Anthropic API functionality, modified styles a bit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,48 @@
 import React from "react"
-import Recipe from "./Recipe"
 import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./Recipe"
+import { getRecipeFromChefClaude } from "../ai"
+
+/**
+ * Challenge: Get a recipe from the AI!
+ * 
+ * This will be a bit harder of a challenge that will require you
+ * to think critically and synthesize the skills you've been
+ * learning and practicing up to this point.
+ * 
+ * Using either the `getRecipeFromChefClaude` function or the 
+ * `getRecipeFromMistral` function, make it so that when the user
+ * clicks "Get a recipe", the text response from the AI is displayed
+ * in the <ClaudeRecipe> component.
+ * 
+ * For now, just have it render the raw markdown that the AI returns,
+ * don't worry about making it look nice yet. (We're going to use a
+ * package that will render the markdown for us soon.)
+ */
 
 export default function Main() {
-
-    const [ingredients, setIngredients] = React.useState(
-        ["all the main spices", "pasta", "ground beef", "tomato paste"]
-    )
-    
+    const [ingredients, setIngredients] = React.useState([])
     const [recipeShown, setRecipeShown] = React.useState(false)
+    
+    const [recipeText, setRecipeText] = React.useState("")
+    
+    async function getRecipe() {
+
+        const recipe = await getRecipeFromChefClaude(ingredients)
+        setRecipeText(recipe)
+        setRecipeShown(true)
+
+    }
+
+    function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    
-    function getRecipe() {
-        setRecipeShown(prevShown => !prevShown)
 
-    }
-    
     return (
         <main>
             <form action={addIngredient} className="add-ingredient-form">
@@ -31,13 +54,19 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {ingredients.length > 0 && 
-            <IngredientsList 
-                ingredients={ingredients}
-                getRecipe={getRecipe}
-            />}
 
-            {recipeShown && <Recipe />}
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    toggleRecipeShown={toggleRecipeShown}
+                    handleClick={getRecipe}
+                />
+            }
+
+            {recipeShown && 
+            <ClaudeRecipe 
+                recipe={recipeText}
+            />}
         </main>
     )
 }
