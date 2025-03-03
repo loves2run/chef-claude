9d1a3442fdbd869ea7515bf593f33a4b13a16326
Brandon Johnson <johnsbr99@gmail.com>
2025-03-03 07:32:58 -0700

    Updated rendering of markdown with react-markdown

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,11 +1,12 @@
+import Markdown from 'react-markdown'
 
-export default function ClaudeRecipe({recipe}) {
+export default function ClaudeRecipe(props) {
     return (
-        <section>
+        <section className='suggested-recipe-container' aria-live='polite'>
             <h2>Chef Claude Recommends:</h2>
-            <article className="suggested-recipe-container" aria-live="polite">
-                {recipe}
-            </article>
+                <Markdown>
+                {props.recipe}
+                </Markdown>
         </section>
     )
 }
